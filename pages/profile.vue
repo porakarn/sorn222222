98d6803d55e55aeb45a831e32c533bb5dee697a1
porakarn <porakarnsoon@gmail.com>
2018-07-06 13:04:05 +0700

    My first commit

--- pages/profile.vue
+++ pages/profile.vue
@@ -4,20 +4,21 @@
     <b-row>
         <b-col cols="4">
   <!-- <b-img rounded="circle" style="width: 70px" :src="this.$store.state.user.picture"/> -->
-    <b-img  style="width: 115px; border-radius: 21px" src="https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.0-1/p240x240/15873069_1211350558943201_5314025003629794223_n.jpg?_nc_cat=0&oh=429da1d277ec885fbbc86409fac1dd86&oe=5BA39AF5"/>
+    <b-img  style="width: 110px; border-radius: 21px" src="https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.0-1/p240x240/15873069_1211350558943201_5314025003629794223_n.jpg?_nc_cat=0&oh=429da1d277ec885fbbc86409fac1dd86&oe=5BA39AF5"/>
 
         </b-col>
          <b-col cols="8">
       <p style="color: rgb(117, 119, 120); font-size: 18px;margin-bottom: 7px;">Porakarn Soonthornsittipong</p>
       <!-- <p style="color: rgb(117, 119, 120);">{{ this.$store.state.user.name}}</p> -->
-      <p style="color: rgb(117, 119, 120);  font-size: 19px; margin-bottom: 0px;"><i style="color: #F1F182;" class="fa fa-star" aria-hidden="true"></i>
+      <p style="color: rgb(158, 158, 158);  font-size: 18px; margin-bottom: 0px;"><i style="color: #F1F182;" class="fa fa-star" aria-hidden="true"></i>
 </i> 4.6 (10)  	&nbsp;&nbsp; <i class="fa fa-user" aria-hidden="true"></i> 12 (30)
   </p>
   <b-row class="my-1">
-    <b-col cols="6" sm="10" style="margin-top: 14px;">
+    <b-col cols="6" sm="10" style="margin-top: 14px;padding-right: 8px;">
       <b-form-input class="formpor" id="input-large"  type="text" placeholder="เบอร์โทร ">{{phone}}</b-form-input>
     </b-col>
-    <b-col cols="6" sm="10" style="margin-top: 14px;">
+    <b-col cols="6" sm="10" style="margin-top: 14px;padding-left: 0px;
+}">
       <b-form-input class="formpor" id="input-large"  type="text" placeholder="Line ">{{phone}}</b-form-input>
     </b-col>
   </b-row>
