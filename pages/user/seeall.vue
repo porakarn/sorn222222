55c9ff3c4d877af3b1d107bdf9b8375447d85d22
porakarn <porakarnsoon@gmail.com>
2018-07-15 12:47:08 +0700

    My first commit

--- pages/user/seeall.vue
+++ pages/user/seeall.vue
@@ -100,8 +100,8 @@
     <div v-for="course in courses" >
 
 
-    <b-card @click="showModal(course)" style="   margin-bottom: 5px;  box-shadow: rgba(119, 118, 118, 0.1) 0px 1px 1px 0px
-;
+    <b-card @click="showModal(course)" style="   margin-bottom: 5px; 
+   box-shadow: rgba(119, 118, 118, 0.1) 0px 1px 11px 0px;
 " 
            > <b-row>  <b-col cols="10" style="    padding-right: 0px;
 ">
