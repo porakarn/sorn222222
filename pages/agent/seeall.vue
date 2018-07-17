40df7b4051170702b0ff1da4774f5842a56a2494
porakarn <porakarnsoon@gmail.com>
2018-07-17 16:37:32 +0700

    My first commit

--- pages/agent/seeall.vue
+++ pages/agent/seeall.vue
@@ -59,7 +59,8 @@
 
 
 
-<b-nav fill tabs style="    background-color: #cdf4d7;" >
+<b-nav fill tabs style="    background-color: #cdf4d7;     box-shadow: 0px 2px 3px -1px rgba(126, 126, 126, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0), 0px 1px 10px 0px #00000003;
+" >
   <b-nav-item  class="responsive"  style="border-bottom: 4px solid;
     border-bottom-color: #ffffff; color: #5b7a69;
 ">ทั้งหมด</b-nav-item>
@@ -468,5 +469,11 @@ border: 1px solid rgba(193, 193, 193, 0.12);
     .responsive2 {
   display: none;
 }
+}
+
+
+a{
+
+  color:#415953;
 }
 </style>
