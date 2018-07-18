bd1850aed7e4ef8e8248b0ffe92eb4056ce2c50a
porakarn <porakarnsoon@gmail.com>
2018-07-18 21:18:47 +0700

    My first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Sorns Community ของการสอน',
+    title: 'Sorns',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'description', name: 'description', content: 'Sorns community ของการสอน' },
       { name: 'theme-color', content: '#cdf4d7' }
 
     ],
