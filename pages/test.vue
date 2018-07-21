c888212c4b2987b5ba9dbe7c5f509d7f38549ab6
porakarn <porakarnsoon@gmail.com>
2018-07-21 20:20:10 +0700

    ddd

---
+++ pages/test.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>sss</div>
+</template>
+
+    
+
+
+
+<script>
+export default {
+     data () {
+      return {
+    
+     
+        e1: 'recent',
+        url:'https://www.facebook.com/v3.0/dialog/oauth?client_id={1164801473596989}&redirect_uri={"https://sorns-co-beta.herokuapp.com/test"}&state={"{st=state123abc,ds=123456789}"}'
+
+      }
+    },
+
+    mounted() {
+     window.location = this.url
+          let code = this.$router.currentRoute.query.code
+          console.log(code);
+          
+
+
+          
+    },
+
+}
+</script>
