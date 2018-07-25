463ebf6e32f6e5bc3543391c2b6b5afff4c4cbda
porakarn <porakarnsoon@gmail.com>
2018-07-25 18:46:52 +0700

    por

--- pages/user/alltutor.vue
+++ pages/user/alltutor.vue
@@ -34,11 +34,11 @@
         </b-col>
          <b-col cols="8" style="margin-left:-5px;padding-left: 0px; padding-right: 2px;">
 <p style=" padding-left: 0px;margin-bottom: 0px; font-weight:600; font-size:17px; margin-top: -6px;">{{tutor.name}}</p>
-<p style="color:#545454; font-size:16px;margin-bottom: 0px;margin-bottom: 2px;
+<p style="color:#545454; font-size:16px;margin-bottom: 0px;margin-bottom: 3px;
 " v-if="tutor.subject">
     <i style=" font-size:15px;       color: #cdf4d7;" class="fa fa-graduation-cap" aria-hidden="true"></i> {{tutor.subject.substring(0,70)}}</p>
 <p style="color:#545454; font-size:16px   ;  margin-bottom: 0px;" v-if="tutor.profile">
-    <i style="font-size:15px;    color: #cdf4d7;" class="fa fa-user" aria-hidden="true"></i> {{tutor.profile.substring(0,65)}}...</p>
+    {{tutor.profile.substring(0,65)}}...</p>
 
         </b-col>
 
