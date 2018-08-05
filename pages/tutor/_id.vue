370a719bdb8e9ef93b9e226fad4fa1c44e1be05f
porakarn <porakarnsoon@gmail.com>
2018-08-05 21:51:52 +0700

    ff

--- pages/tutor/_id.vue
+++ pages/tutor/_id.vue
@@ -60,7 +60,8 @@
  ">ติดต่อ</b-button>  
 </div>
 
- <social-sharing :url="url"
+<br>
+ <!-- <social-sharing :url="url"
                       title="The Progressive JavaScript Framework"
                       description="Intuitive, Fast and Composable MVVM for building interactive interfaces."
                       quote="Vue is a progressive framework for building user interfaces."
@@ -68,53 +69,24 @@
                       twitter-user="vuejs"
                       inline-template>
   <div>
-      <network network="email">
-          <i class="fa fa-envelope"></i> Email
-      </network>
+    
       <network network="facebook">
         <i class="fa fa-facebook"></i> Facebook
       </network>
-      <network network="googleplus">
-        <i class="fa fa-google-plus"></i> Google +
-      </network>
+     
       <network network="line">
         <i class="fa fa-line"></i> Line
       </network>
-      <network network="linkedin">
-        <i class="fa fa-linkedin"></i> LinkedIn
-      </network>
-      <network network="odnoklassniki">
-        <i class="fa fa-odnoklassniki"></i> Odnoklassniki
-      </network>
-      <network network="pinterest">
-        <i class="fa fa-pinterest"></i> Pinterest
-      </network>
-      <network network="reddit">
-        <i class="fa fa-reddit"></i> Reddit
-      </network>
-      <network network="skype">
-        <i class="fa fa-skype"></i> Skype
-      </network>
-      <network network="sms">
-        <i class="fa fa-commenting-o"></i> SMS
-      </network>
-      <network network="telegram">
-        <i class="fa fa-telegram"></i> Telegram
-      </network>
+      
+      
+    
       <network network="twitter">
         <i class="fa fa-twitter"></i> Twitter
       </network>
-      <network network="vk">
-        <i class="fa fa-vk"></i> VKontakte
-      </network>
-      <network network="weibo">
-        <i class="fa fa-weibo"></i> Weibo
-      </network> 
-      <network network="whatsapp">
-        <i class="fa fa-whatsapp"></i> Whatsapp
-      </network>
+    
+   
   </div>
-</social-sharing>
+</social-sharing> -->
    <br>
   <b-row> <b-col >
        
