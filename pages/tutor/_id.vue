c61e5a451852c8a1eed5bc819a6eb7e27f78f14d
porakarn <porakarnsoon@gmail.com>
2018-08-06 21:18:21 +0700

    dds

--- pages/tutor/_id.vue
+++ pages/tutor/_id.vue
@@ -146,7 +146,7 @@ color: #484848; border:1px !important;">โปรไฟล์ของคุณ<
     font-size: 16px;
     margin-right: 10px;  ">{{review.studentid.name.split(" ").slice(0, -1).join(" ")}}   </strong>  
 
- <star-rating  border-color="#d8d8d8"  v-bind:increment="0.50"  v-model="review.rating" v-bind:show-rating="false" v-bind:star-size="20"  :border-width="1"  :rounded-corners="true" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"
+ <star-rating  v-bind:read-only="true"	 border-color="#d8d8d8"  v-bind:increment="0.50"  v-model="review.rating" v-bind:show-rating="false" v-bind:star-size="20"  :border-width="1"  :rounded-corners="true" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"
    ></star-rating> <strong style="padding-left: 3px; font-weight: 400">{{review.rating}}</strong>
 
  <strong style="    color: rgb(165, 202, 228);
