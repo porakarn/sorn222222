c1c374c0c75dd98d7f2d974bc0538a828c40638e
porakarn <porakarnsoon@gmail.com>
2018-11-18 22:22:06 +0700

    sd

---
+++ pages/student/่jobform/purpose.vue
@@ -0,0 +1,186 @@
+<template>
+<b-container fluid style="padding: 0px;">
+
+<b-navbar  style="  box-shadow: 0px 2px 3px -1px rgba(126, 126, 126, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0), 0px 1px 10px 0px #00000003;   padding-bottom: 15px;
+    padding-top: 15px;  background-color:#DFAEA9!important; "toggleable variant="light">
+     <i @click="goback" style=" font-size: 25px;
+    padding-left: 4px;
+    color: white;
+" class="fa fa-arrow-left responsive" aria-hidden="true"></i>
+        <!-- <b-navbar-brand>BootstrapVue</b-navbar-brand> -->
+   
+        
+       
+       
+    </b-navbar>
+
+
+<b-container fluid>
+  <br><br>
+  
+  <b-form-group label="Stacked radios">
+      <b-form-radio-group v-model="selected"
+                          :options="options"
+                          stacked
+                          name="radiosStacked">
+      </b-form-radio-group>
+    </b-form-group>
+
+
+
+
+
+<br><br>
+
+ <div  style="text-align :center">       <nuxt-link class="nav-item" style="color:#345d46; " to="/student/createjob">   <b-button 
+   style="background-color: #EFBAB5; border: 0px;     padding-left: 64px;
+    padding-right: 64px;
+    
+    padding-top: 11px;
+    padding-bottom: 11px;
+    border-radius: 22px;">ลงประกาศหาติวเตอร์</b-button></nuxt-link> </div> 
+</b-container>
+</b-container>
+</template>
+
+<script>
+import axios from 'axios';
+import moment from 'moment';
+
+
+ export default {
+    data(){
+    return {
+      tutors:{},
+       loading: false,
+           profile2: false,
+           selected: 'first',
+      options: [
+        { text: 'First radio', value: 'first' },
+        { text: 'Second radio', value: 'second' },
+        { text: 'Third radio', value: 'third' }
+      ],
+    
+
+    }
+},
+ 
+
+
+mounted() {
+
+
+},
+//   computed: {
+//     calculateScore (x) {
+//      console.log(x);
+     
+// // this.sum = x.reduce(function(a, b) {
+// //   return a + b;
+// // });
+//     }
+//   },
+
+
+
+  methods: {
+  goback(){
+            this.$router.go(-1)
+ this.$router.push('/student/demo2')
+        },
+
+    sendout(){
+this.loading = true
+  this.$nuxt.$loading.start()
+    
+            console.log(this.selected);
+         
+
+let createPost = {
+      
+    
+  
+        subject :this.selected,
+     
+
+       
+
+      }
+      console.log(createPost);
+      
+
+      if (this.selected.length > 0) {
+           axios.post('https://frozen-mesa-40722.herokuapp.com/tutor/filter', createPost)
+                // axios.get('http://localhost:8000/job/all')
+
+          .then((res) => { 
+              
+              console.log(res.data)
+            //   this.courses = res.data
+              this.tutors = res.data,
+             this.$refs.myModalRef4.hide()
+             this.loading = false
+               this.$nuxt.$loading.finish()
+
+          })
+          .catch(error => console.log(error))
+      } else {
+            axios.get('https://frozen-mesa-40722.herokuapp.com/tutor/all')
+                // axios.get('http://localhost:8000/job/all')
+
+          .then((res) => { 
+              
+              console.log(res.data)
+            //   this.courses = res.data
+              this.tutors = res.data,
+             this.$refs.myModalRef4.hide()
+             this.loading = false
+               this.$nuxt.$loading.finish()
+
+          })
+          .catch(error => console.log(error))
+      }
+
+      
+    },
+
+
+   seeeach(x){
+ 
+
+       if (this.$store.state.isUserLoggedIn) {
+ this.$router.push(`/tutor/${x}`)
+       } else{
+                                    this.$refs.myModalRef.show() 
+
+       }
+       
+   },
+
+       displayTimestamp(t){
+                return moment(t).locale('th').fromNow()
+            },
+            nextPage(){
+    this.$router.push('/tutor/sheetall')
+},
+nextPage2(){
+    this.$router.push('/tutor/job')
+
+
+}, 
+
+ 
+
+
+
+                         // axios.get('http://localhost:8000/job/all')
+
+     
+}
+,
+    layout: 'studentapp'
+
+
+
+}
+</script>
