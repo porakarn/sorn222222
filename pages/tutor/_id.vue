6294662ce27b25b92a0ddaeed227b7d6d10f5f07
porakarn <porakarnsoon@gmail.com>
2018-11-25 22:01:35 +0700

    dffd

--- pages/tutor/_id.vue
+++ pages/tutor/_id.vue
@@ -1,8 +1,10 @@
 <template>
 <b-container fluid style="padding: 0px;">
 
-<b-navbar  style="  box-shadow: 0px 2px 3px -1px rgba(126, 126, 126, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0), 0px 1px 10px 0px #00000003;   padding-bottom: 15px;
-    padding-top: 15px;  background-color:#DFAEA9!important; "toggleable variant="light">
+<b-navbar  style="     border: 3px solid #DFAEA9;
+    padding-bottom: 13px;
+    padding-top: 15px;
+    background-color: #DFAEA9!important; "toggleable variant="light">
      <i @click="goback" style=" font-size: 25px;
     padding-left: 4px;
     color: white;
@@ -30,7 +32,7 @@
 
 
 <b-container fluid style="padding:0px">
-  <div style="background-color:#f7f7f7; height: 130px; margin-bottom: -110px;">
+  <div style="   background-color: #dfaea9;; height: 130px; margin-bottom: -110px;">
 
   </div>
 <b-container fluid>
