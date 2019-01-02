1c8b114cd136a11abaad404706078c7bfa84fc1c
porakarn <porakarnsoon@gmail.com>
2019-01-02 15:55:04 +0700

    ssd

---
+++ pages/homepagetest.vue
@@ -0,0 +1,123 @@
+<template>
+    <b-container fluid>
+        <br>
+
+        <div style="text-align:center" >
+                     <img src="/Sornstutorlogo.png" style="border-radius: 5px" height="90"> 
+</div>
+        <div style="text-align: center;">
+         <h1 style="font-size: 20px; margin-top: 10px;">ค้นหาติวเตอร์ตัวต่อตัว</h1>
+
+   
+<br>
+    <!-- <b-form-select v-model="selected" :options="options" class="mb-3" /> -->
+<b-form-group  id="exampleInputGroup3"
+                  
+                    label-for="exampleInput3">
+        <b-form-select  class="formpor" id="exampleInput3" size="lg"
+                      :options="options"
+                    
+                      v-model="selected">
+        </b-form-select>
+      </b-form-group>
+<b-form-group  id="exampleInputGroup3"
+                  
+                    label-for="exampleInput3">
+        <b-form-select  class="formpor" id="exampleInput3" size="lg"
+                      :options="locations"
+                    
+                      v-model="location">
+        </b-form-select>
+      </b-form-group>
+
+ <b-button @click="totutor" size="lg" style="  background-color: #DFAEA9; border: 0px; padding-left: 66px;
+    padding-right: 66px;">ค้นหา</b-button>  
+        <!-- <b-button size="lg" style="  background-color: white; border: 0px; padding-left: 66px; color:#33C1C1;
+    padding-right: 66px;">สมัครนักเรียน</b-button>   -->
+<br><br>
+     
+</div>
+<br>
+
+
+
+   </b-container>
+</template>
+
+
+
+<script>
+
+export default {
+    data () {
+        
+    return {
+          selected: null,
+   
+        options: [
+        { text: 'วิชาที่สนใจเรียน', value: null },
+       'ชาย', 'หญิง',
+      ],
+         location: null,
+   
+        locations: [
+        { text: 'สถานที่', value: null },
+       'ชาย', 'หญิง',
+      ],
+      text1: ''
+    }
+  },
+   methods: {
+     totutor(){
+ this.$router.push('/user/fblogin')
+
+     },
+    logout () {
+       this.$store.dispatch('setToken', null)
+              this.$store.dispatch('setAgent', null)
+
+      this.$store.dispatch('setUser', null)
+       this.$store.dispatch('setStudent', null)
+
+            // this.$store.dispatch('setProfile', null)
+
+    }
+  },
+  
+           layout: 'auth'
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title
+{
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle
+{
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links
+{
+  padding-top: 15px;
+}
+
+
+</style>
---
+++ pages/newjob/myjob.vue
@@ -0,0 +1,243 @@
+<template>
+<b-container fluid style="padding: 0px;">
+
+<b-container fluid>
+
+  <b-row>
+<b-col md="6" offset-md="3">
+    <br>
+
+
+    <b-row>
+       
+       
+
+<b-row>
+<!-- <b-col cols="4" style="    padding-right: 0px;">
+<p style="  font-size :18px;
+     text-align: center; font-weight: bold; margin-bottom: 0px;">4.6</p>
+<p style=" 
+   color:#949494;  text-align: center;  font-size:14px;  margin-bottom: 0px;"><i style="color: #F1F182;" class="fa fa-star" aria-hidden="true"></i> รีวิว</p>
+</b-col> -->
+<!-- <b-col cols="4">
+<p style="  font-size :18px;
+     text-align: center; font-weight: bold; margin-bottom: 0px;">12</p>
+<p style=" 
+   color:#949494;   text-align: center; font-size:14px;  margin-bottom: 0px;">นักเรียน</p>
+</b-col> -->
+<!-- <b-col cols="4" style="padding-left: 0px;"    >
+<p style="  font-size :18px;
+     text-align: center; font-weight: bold;  margin-bottom: 0px;">12</p>
+<p style=" 
+    color:#949494;  text-align: center; font-size:14px;   margin-bottom: 0px;">ครั้งที่สอน</p>
+</b-col> -->
+
+
+</b-row>
+
+      <!-- <p style="color: rgb(117, 119, 120);">{{ this.$store.state.user.name}}</p> -->
+      <!-- <p style="color:#484848;  font-size: 16px;     margin-bottom: 0px;
+    margin-top: 12px;
+ "><i style="color: #F1F182;" class="fa fa-star" aria-hidden="true"></i>
+</i> 4.6 (10)  	&nbsp;&nbsp; <i class="fa fa-user" aria-hidden="true"></i> 12 (30 ครั้ง)
+  </p> -->
+  <!-- <nuxt-link to="/tutor/profile/edit"> -->
+  <b-button size="lg" @click="onSubmit" style="background-color: #DFAEA9; border-radius:25px;     margin-top: 9px;  box-shadow: 0 1px 1px 1px rgba(111, 111, 111, 0.23);
+; border: 0px;      padding-top: 12px;
+    padding-bottom: 12px;   width: -webkit-fill-available;">สร้างประกาศหาติวเตอร์</b-button>
+<!-- </nuxt-link> -->
+
+  
+  <!-- <p  v-if="isUserLoggedIn"
+> {{this.$store.state.user.name}} </p>
+ <p  v-if="this.$store.state.haveProfile"
+> {{this.$store.state.user.subject}} </p> -->
+             
+    </b-row>
+
+   <br>
+ 
+
+
+
+  </div>
+     </b-col></b-row>
+  <br><br><br> 
+</b-col></b-row>
+</b-container>
+
+
+
+<b-nav fill tabs style=" position: fixed;    width:100%;
+  border-top:1px solid;
+  border-top-color:rgba(241, 242, 243, 1);
+  border-color: #e2e5e957;
+  bottom: 44px; 
+  background-color: white;
+  height: 58px;">
+  <b-nav-item  >   <nuxt-link to="/tutor/givelink"> ให้นักเรียนรีวิว </nuxt-link>  </b-nav-item>
+ <b-nav-item @click="nextPage">งานของคุณ</b-nav-item>
+ 
+</b-nav>
+</b-container>
+</template>
+
+<script>
+import axios from 'axios';
+import moment from 'moment'
+
+
+export default {
+  
+   data () {
+    return {
+    course: {},
+    phone: '',
+    profile: '',
+    text2: '',
+    text:'',
+    subject:'',
+    reviews:'',
+    name:'',
+    picture: '',
+    location: '',
+  
+    }
+  },
+  
+ 
+  mounted() {
+
+  },
+    computed: {
+    isUserLoggedIn () {
+      return this.$store.state.isUserLoggedIn
+    }
+  },
+//   asyncData (context, callback) {
+//  var por = ';;'
+//    return   axios.get('https://frozen-mesa-40722.herokuapp.com/user/profile', por )
+//     .then((res) => {
+//       // this.profile = res.data.profile
+//       // this.phone = res.data.phone                
+         
+//             })
+    
+//   }
+//   ,
+    methods: {
+       displayTimestamp(t){
+                return moment(t).fromNow()
+            },
+
+      porakarn(){
+    
+    var data = {
+      tutorid : this.$store.state.user._id
+    }
+  // axios.post('http://localhost:8000/user/profile2', data).then((res) =>{
+            axios.post('https://frozen-mesa-40722.herokuapp.com/user/profile2', data).then((res) =>{
+
+            console.log(res.data)
+             this.reviews = res.data._review
+            
+
+           }).catch((error) =>{ console.log(error) })
+           
+
+      },
+      nextPage(){
+             this.$router.push('/tutor/job')
+      }
+      ,
+
+      onSubmit(){
+        axios.post('http://localhost:8000/sms').then((res) => { 
+              
+              console.log(res.data)
+             
+  
+          })
+          .catch(error => console.log(error))
+
+           var data = {
+             profile : this.profile, 
+             phone : this.phone,
+             name: this.$store.state.user.username 
+              
+           }
+
+           axios.patch('https://frozen-mesa-40722.herokuapp.com/update', data).then((res) =>
+           {
+            console.log(res.data);
+            
+
+           }).catch((error) =>{ console.log(error) })
+           
+
+      }
+
+  },
+    layout: 'studentapp'
+
+}
+</script>
+
+
+
+<style scoped>
+<style scoped>
+::placeholder {
+    color: #C9C9C9;
+    opacity: 1; /* Firefox */
+}
+
+.formpor{
+border-top: 0px;
+    border-right: 0px;
+    border-left: 0px;
+    border-radius: 0;
+    padding-left: 4px;
+}
+
+.card-link{
+        margin-left: 7px;
+         color: #ffffff;
+    text-decoration: none;
+    background-color: #33C1C1;
+    padding: 5px;
+    padding-left: 9px;
+    padding-right: 9px;
+    border-radius: 4px;
+    font-size: 18px;
+
+}
+
+.form-control:focus {
+    color: #495057;
+    background-color: #fff;
+    border-color: #80bdff;
+    outline: 0;
+box-shadow: 0 0 0 0rem rgba(0, 123, 255, .25) !important;
+}
+
+.btn-secondary:not(:disabled):not(.disabled):active:focus, .btn-secondary:not(:disabled):not(.disabled).active:focus, .show > .btn-secondary.dropdown-toggle:focus {
+    box-shadow: 0 0 0 0.2rem rgba(255, 255, 255, 0.5);
+}
+
+.card1{
+    margin-bottom:0px;  
+     border: 0px solid;  
+        border-bottom-width: 1px;
+    border-bottom: 1px solid whitesmoke !important;  
+     margin-bottom:5px;  
+       padding-top: 1px;
+    padding-bottom: 1px;
+ 
+
+    border-radius: 6px;      
+   
+}
+
+
+</style>
