cd0c95f89fe6eaaf1cbba503009f81eab1e8bed1
porakarn <porakarnsoon@gmail.com>
2019-01-06 13:47:55 +0700

    sd

---
+++ pages/student/formcreatejob/index.vue
@@ -0,0 +1,390 @@
+<template>
+<b-container fluid style="padding:0px">
+ 
+<b-container fluid>
+<br><br>
+  <div> <div class="my-3">
+ 
+</div>
+          <form @submit.prevent="onCreate">
+<b-row>
+  <b-col offset-lg="3" lg="6">
+              <div v-if="step === 1">
+
+  <div  style="text-align :center; "> 
+<b-form-group  id="exampleInputGroup3"
+                  
+                    label-for="exampleInput3">
+        <b-form-select style="     font-size: 17px;
+    color: #858484;   border-radius: 5px; 
+    
+        border: 0px;
+    box-shadow: 0 1px 3px #74686833;" class="formpor" id="exampleInput3" size="lg"
+                      :options="options"
+                    
+                      v-model="selected">
+        </b-form-select>
+      </b-form-group>
+ </div>
+ <!-- <b-form-select  :select-size="2" v-model="selected2" :options="options2" class="mb-3">
+    </b-form-select> -->
+ <div  style="text-align :center"> 
+  <b-form-group v-if="selected"  id="exampleInputGroup1"
+                    
+                    label-for="exampleInput1"
+                    description=" ตัวอย่าง ภาษาอังกฤษ GAT ENG">
+        <b-form-input style="border-radius:5px;  
+      font-size: 17px;
+    border: 0px;    
+    box-shadow: 0 1px 3px #74686833;   padding-top: 10px;
+    padding-bottom: 10px;" class="formpor" id="exampleInput1" size="lg" 
+                      type="text"
+                      required
+                      v-model="subjectDetail"
+                      placeholder="รายละเอียดวิชา">
+        </b-form-input>
+      </b-form-group>
+ </div>
+
+
+
+  <!-- <b-form-textarea style="  white-space: pre-wrap;   padding-top: 0px;
+" class="formpor" id="textarea1"
+                     
+                     placeholder=""
+                     :rows="2"
+                     :max-rows="6">
+    </b-form-textarea> -->
+
+<br><br>
+<!-- <button @click="testbtn">Test</button> -->
+
+
+<br><br><br><br>
+ <div  style="text-align :center">        <b-button  @click.prevent="next()"
+   style="background-color: #EFBAB5; border: 0px;   
+    width:100%;
+    
+    padding-top: 13px;
+    padding-bottom: 13px;
+    border-radius: 4px;">เรียบร้อย</b-button> </div> 
+<br><br><br><br><br>
+    
+ 
+    
+
+              </div>
+
+ <div v-if="step === 2">
+ <b-form-group style=" " label="จุดประสงค์ของการเรียน">
+      <b-form-radio-group v-model="selected_purpose"
+                          :options="options_purpose"
+                          plain
+                          stacked
+                          name="plainStacked" />
+    </b-form-group>
+
+ <!-- <b-form-select  :select-size="2" v-model="selected2" :options="options2" class="mb-3">
+    </b-form-select> -->
+
+  <b-form-group v-if="selected"  id="exampleInputGroup1"
+                    
+                    label-for="exampleInput1"
+                    description="เช่น ต้องการสอบเข้าเตรียมอุดม">
+        <b-form-input  class="formpor" id="exampleInput1" size="lg" 
+                      type="text"
+                      required
+                      v-model="form"
+                      placeholder="อื่นๆ">
+        </b-form-input>
+
+      </b-form-group>
+
+
+
+<br><br>
+
+
+ <div  style="text-align :center">       <b-button  @click.prevent="next()"
+   style="background-color: #EFBAB5; border: 0px;  width:80%
+    
+    padding-top: 11px!important;
+    padding-bottom: 11px!important;
+    border-radius: 4px;">เรียบร้อย</b-button> 
+ </div>
+        <b-button style="background-color: #33C1C1; border: 0px; padding-left: 46px;
+    padding-right: 46px;" @click.prevent="next()">ต่อไป</b-button>
+
+        <b-button style="background-color: white; border: 0px; padding-left: 36px;  color: grey;
+    padding-right: 36px;" @click.prevent="prev()">กลับ</b-button>
+
+    </div>
+
+
+    <div v-if="step === 3">
+
+ <b-form-group >
+      <b-form-radio-group v-model="selected_level"
+                          :options="options_level"
+                          plain
+                          stacked
+                          name="plainStacked" />
+    </b-form-group>
+
+        <b-button style="background-color: #33C1C1; border: 0px; padding-left: 46px;
+    padding-right: 46px;" @click.prevent="next()">ต่อไป</b-button>
+
+        <b-button style="background-color: white; border: 0px; padding-left: 36px;  color: grey;
+    padding-right: 36px;" @click.prevent="prev()">กลับ</b-button>
+
+    </div>
+
+   <div v-if="step === 4">
+
+        <b-form-group  id="exampleInputGroup1"
+                    
+                    label-for="exampleInput1"
+                    description=" ตัวอย่าง ค่าสอน 300/ชม ค่าแนะนำ 600 บาท (ถ้ามี)">
+        <b-form-input class="formpor" id="exampleInput1" size="lg" 
+                      type="text"
+                      v-model="form.tuitionfee"
+                      required
+                      placeholder="ค่าสอน ค่าแนะนำ">
+        </b-form-input>
+      </b-form-group>
+
+       <b-form-group  id="exampleInputGroup1"
+                    
+                    label-for="exampleInput1"
+                    description=" เช่น ขอติวเตอร์หญิง">
+        <b-form-input id="exampleInput1" size="lg"  class="formpor"
+                      type="text"
+                      v-model="form.note"
+                      required
+                      placeholder="หมายเหตุเพิ่มเติม (ถ้ามี)">
+        </b-form-input>
+      </b-form-group>
+       <b-button  type="submit" style="background-color: #33C1C1; border: 0px; padding-left: 46px;
+    padding-right: 46px;">เรียบร้อย</b-button>
+         <b-button style="background-color: white; border: 0px; padding-left: 36px; color: grey;
+    padding-right: 36px;" @click.prevent="prev()">กลับ</b-button>
+            </div>
+              <div class="loading-page" v-if="loading">
+    <p>Loading...</p>
+  </div>
+    
+
+    
+    <br>
+  </b-col></b-row>
+          </form>
+          <div class="loading-page" v-if="loading2">
+    <p style="color: #54c686; margin-top: 7px;"><i class="fa fa-check-circle" aria-hidden="true"></i>
+ เรียบร้อย</p>
+  </div>
+  </div>
+
+<b-modal  hide-header  hide-footer  centered ref="myModalRef" hide-footer title="Using Component Methods">
+      <div class="d-block ">
+         <div style="white-space: pre-wrap;">{{str5}}</div> 
+      </div>  <br>
+          <b-button @click="onSubmit" style="background-color: #33C1C1; border: 0px; padding-left: 46px;
+    padding-right: 46px;">ยืนยัน</b-button> <b-button style="background-color: white; border: 0px; padding-left: 36px; color: grey;
+    padding-right: 36px;" @click="hideModal">แก้ไข</b-button>
+    </b-modal>
+</b-container>
+</b-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      loading: false,
+      loading2: false,
+    
+       step:1,
+       str5:'',
+       subject:'',
+      job: '',
+      day: '',
+       form: {
+        email: '',
+        subject: '',
+        note: '',
+        tuitionfee: '',
+        school: '',
+        dayandtime: '',
+        location: '',
+        food: null,
+        gender: null,
+        checked: []
+      },
+      foods: [
+        { text: 'ระดับชั้น', value: null },
+       'มหาลัย', 'ม.6', 'ม.5', 'ม.4', 'ม.3', 'ม.2', 'ม.1', 'ป.6','ป.5','ป.4','ป.3','ป.2','ป.1','อนุบาล','อื่นๆ',
+      ],
+       genders: [
+        { text: 'เพศนักเรียน', value: null },
+       'ชาย', 'หญิง',
+      ],
+       selected: null,
+      options: [
+        { value: null, text: 'วิชาที่อยากเรียน' },
+        { value: 'ENG', text: 'ENG' },
+        { value: 'คณิต', text: 'คณิตศาสตร์' },
+        { value: 'วิทย์', text: 'วิทยาศาสตร์' },
+        { value: 'ไทย', text: 'ไทย' },
+        { value: 'สังคม', text: 'สังคม' },
+        { value: 'ฟิสิกส์', text: 'ฟิสิกส์' },
+        { value: 'เคมี', text: 'เคมี' },
+        { value: 'ชีวะ', text: 'ชีวะ' },
+        { value: 'จีน', text: 'ภาษาจีน' },
+        { value: 'ญี่ปุ่น', text: 'ภาษาญี่ปุ่น' },
+        { value: 'เกาหลี', text: 'ภาษาเกาหลี' },
+        { value: 'อื่นๆ', text: 'อื่นๆ' },
+
+    
+      ],
+        selected_purpose: null,
+      options_purpose: [
+      
+        { value: 'เพิ่มความรู้', text: 'เพิ่มความรู้' },
+        { value: 'สอบเข้าเรียนต่อ', text: 'สอบเข้าเรียนต่อ' },
+        { value: 'เพิ่มเกรด ', text: 'เพิ่มเกรด' },
+        { value: 'สอบวัดระดับต่างๆ', text: 'สอบวัดระดับต่างๆ' },
+        { value: 'อื่นๆ', text: 'อื่นๆ' },
+       
+
+    
+      ],
+       selected_level: null,
+      options_level: [
+      
+        { value: 'อนุบาล', text: 'อนุบาล' },
+        { value: 'ประถม', text: 'ประถม' },
+        { value: 'มัธยมต้น', text: 'มัธยมต้น' },
+        { value: 'มัธยมปลาย', text: 'มัธยมปลาย' },
+        { value: 'มหาลัย', text: 'มหาลัย' },
+        { value: 'บุคคลทั่วไป', text: 'บุคคลทั่วไป' },
+
+    
+      ],
+  
+    }
+  },
+
+methods: {
+    prev() {
+      this.step--;
+    },
+    next() {
+      this.step++;
+    },
+    nextPage(){
+   this.$router.push('/agent/create')
+
+    }
+
+   ,
+     hideModal () {
+      this.$refs.myModalRef.hide()
+    },
+    onCreate() {
+       this.$nuxt.$loading.start()
+              this.loading = true
+      this.str5 = this.form.subject + ' ' + this.form.note +'\n- '+ this.form.gender + ' ' + this.form.food + ' (' + this.form.school +') \n- '+this.form.location +'\n- '+this.form.dayandtime +'\n- '+this.form.tuitionfee
+    // alert(this.str5)
+        this.$refs.myModalRef.show() 
+    },
+    onSubmit(){
+    
+
+  let createPost = {
+      
+        job: this.str5,
+        _creator: this.$store.state.agent._id,
+        day : day,
+        subject : subject,
+        contact: this.$store.state.agent.contact,
+        line: this.$store.state.agent.line,
+        creator_name : this.$store.state.agent.pagename,
+         picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlxhsGV-_-Fxr0-74drOlYw5pi21K32Hq7FZMfk9VzAZTrvLJufg',
+         is_student: 'Agent'
+
+      }
+   console.log(createPost);
+   
+   
+        axios.post('https://frozen-mesa-40722.herokuapp.com/job/create', createPost)
+          .then((res) => { 
+              
+              console.log(res.data)
+                  
+   this.$refs.myModalRef.hide()
+     
+       this.form.email = ''
+       this.form.subject = ''
+        this.form.note = ''
+        this.form.tuitionfee = ''
+        this.form.school = ''
+       this.form.dayandtime = ''
+        this.form.location = ''
+        this.form.food = null
+        this.form.gender = null
+     
+              this.loading = false
+               this.loading2 = true
+                this.$nuxt.$loading.finish()
+
+  
+          })
+          .catch(error => console.log(error))
+    
+    }
+},
+     layout: 'auth'
+
+}
+</script>
+      
+<style scoped>
+
+
+
+.formpor{
+border-top: 0px;
+    border-right: 0px;
+    border-left: 0px;
+    border-radius: 0;
+    padding-left: 4px;
+}
+
+
+.form-group {
+  
+    margin-bottom: 30px;
+}
+
+.btn-secondary:focus, .btn-secondary.focus {
+    box-shadow: 0 0 0 0.2rem rgba(255, 255, 255, 0.5);
+}
+
+.form-control:focus {
+    color: #495057;
+    background-color: #fff;
+    border-color: #80bdff;
+    /* border-bottom: 2px; */
+    outline: 0;
+    box-shadow: 0px 1px 0px 0rem rgba(199, 199, 199, 0.25);
+}
+
+a{
+ color: #4159539c;
+    font-weight: 600;
+    font-size: 18px;
+    font-family:"Sukhumvit Set",sans-serif; 
+}
+</style>
